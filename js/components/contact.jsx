fc0a498176c2cbe5c0945f29e9f7dc8bd1d17213
Małgorzata Grajewska <malgorzata.grajewska@hotmail.co.kr>
2017-11-06 14:49:37 +0100

    swiched i with span

--- js/components/contact.jsx
+++ js/components/contact.jsx
@@ -81,17 +81,17 @@ export class Contact extends React.Component {
             <h2>Get in touch</h2>
             <form id={scss.contact__form} onSubmit={this.sendMessage}>
                     <div id={scss.name}>
-                        <i className="fa fa-user  fa-lg" aria-hidden="true"></i>
+                        <span className="fa fa-user  fa-lg" aria-hidden="true"></span>
                         <input id='name' className={scss.input__name} name='name' type='text' onChange={this.nameChange} value={this.state.name} required/>
                         <label className={scss.label__name} htmlFor='name'>Your name</label>
                     </div>
                     <div id={scss.email}>
-                        <i className="fa fa-envelope  fa-lg" aria-hidden="true"></i>
+                        <span className="fa fa-envelope  fa-lg" aria-hidden="true"></span>
                         <input id='email' className={scss.input__email} name='email' type='email' value={this.state.email} onChange={this.emailChange}required/>
                         <label  className={scss.label__email} htmlFor='email'>Your email</label>
                     </div>
                     <div id={scss.message}>
-                        <i className="fa fa-pencil  fa-lg" aria-hidden="true"></i>
+                        <span className="fa fa-pencil  fa-lg" aria-hidden="true"></span>
                         <textarea id='message' className={scss.message} onChange={this.messageChange} value={this.state.message} required></textarea>
                         <label  className={scss.label__textarea} htmlFor='message'>Type your message</label>
                     </div>
