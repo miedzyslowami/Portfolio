15e2f84d24d98b2e4ba0570e3efb70ece95893b7
Małgorzata Grajewska <malgorzata.grajewska@hotmail.co.kr>
2019-06-14 14:26:34 +0200

    Add production configuration

---
+++ webpack.production.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+   entry: {
+     app: './js/app.js'
+   },
+  devtool: 'source-map',
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'public')
+   },
+
+     plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({ template: 'index.html' })
+   ],
+   module: {
+     rules: [
+
+       {
+         test: /\.jsx$/,
+         exclude: /node_modules/,
+         loader: 'babel-loader',
+         options:{
+           presets:['env','react','stage-2']
+         }
+       },
+       {
+       test: /\.html$/,
+       loader: 'html-loader'
+     }
+
+     ]
+   }
+ }
