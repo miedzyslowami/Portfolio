2409acc317de0f8bf9bd5746a1073444d3bf2618
Małgorzata Grajewska <malgorzata.grajewska@hotmail.co.kr>
2019-06-17 14:56:43 +0200

    Add performance audit report starting point

--- src/js/components/about.jsx
+++ src/js/components/about.jsx
@@ -36,7 +36,7 @@ export class About extends React.Component {
         return (
             <section name='About' id={scss.about}>
                 <h2>Know me</h2>
-                <img src='http://res.cloudinary.com/miedzyslowamicode/image/upload/v1509381717/Assets/portfolio_about_me.png' alt='picture of me'/>
+                <img src='https://res.cloudinary.com/miedzyslowamicode/image/upload/v1509381717/Assets/portfolio_about_me.png' alt='picture of me'/>
                 <div className={scss.info}>
                     <p>I'm Gosia and I am React's enthusiast, but not only.</p>
                     <p>I love traveling and learning new stuff. You can tell that by checking my <a target='_blank' href='https://www.linkedin.com/in/miedzyslowami/'>linkedin profile</a>. I manage my time well and I learn fast. I constantly develop my skills, what you can observe at my <a href='https://github.com/miedzyslowami' target='_blank'>github profile</a>.
